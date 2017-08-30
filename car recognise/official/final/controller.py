16038f211d997953b20990e5ec988834c1077f0b
akshat <akshatmehrotra2004@gmail.com>
2017-08-30 21:17:44 +0530

    Update controller.py

--- car recognise/official/final/controller.py
+++ car recognise/official/final/controller.py
@@ -11,16 +11,16 @@ def main():
     coordinates = []
 
     for j in sections:
-        for i in ('row start', 'row end', 'column start', 'column end'):
+        for i in ('top left x', 'top left y', 'width', 'height'):
             c = config[j]
             try:
                 coordinates.append(int(c.get(i)))
             except ValueError:
                 print('hello')
                 break
-        cam_num = config[j]['cam num']                                  # this is the cam number  the stuff bellow this
+        cam_num = int(config[j]['cam num'])                                  # this is the cam number  the stuff bellow this
         print(cam_num, cam_num)
-        alt_feed = 'other_files/yh.avi'
+        alt_feed = 'other_files/video1.avi'
         thread_counter.append(threading.Thread(target=cars_cam.init, args=(cam_num, alt_feed, coordinates, j,)))
     print(coordinates)
     for i in thread_counter:
