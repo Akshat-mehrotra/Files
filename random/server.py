b7cbe99ccf5cbeb92ef14093f090023ea3dc8a2d
akshat <akshatmehrotra2004@gmail.com>
2017-09-30 20:33:00 +0530

    Update server.py

--- random/server.py
+++ random/server.py
@@ -1,19 +1,21 @@
 import socket
 
 s = socket.socket()
-s.bind(("It's ye boi", 1974))
+s.bind(('', 8080))
 s.listen(10)
 
 while True:
     sc, add = s.accept()
-
-    with open('something.mp4', 'wb') as f:
+    print(add)
+    with open('image.jpg', 'wb') as f:
         while True:
             l = sc.recv(4000)
             while l:
                 f.write(l)
                 l = sc.recv(4000)
+            break
+    break    
     sc.close()
-
+    print('done')
 s.close()
 print('done')
