593aabe0147869b7529705cc9273a94ca2f68a0d
akshat <akshatmehrotra2004@gmail.com>
2017-09-30 12:52:32 +0530

    Update client.py

--- random/client.py
+++ random/client.py
@@ -2,7 +2,7 @@ import socket
 
 s = socket.socket()
 s.connnect(("It's ye boi", 1974))
-with open('movie.mov', 'rb') as f:
+with open('movie.mp4', 'rb') as f:
     l = f.read(4000)
     while l:
         s.send(l)
