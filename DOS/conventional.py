1ca9cb117a0e185fd8d76f224a0d3b29dbe5a2e4
akshat <akshatmehrotra2004@gmail.com>
2018-01-21 13:35:38 +0530

    Update conventional.py

--- DOS/conventional.py
+++ DOS/conventional.py
@@ -4,13 +4,13 @@ import time
 from queue import Queue
 import urllib3.request
 
-
+host = ''
 print_lock = threading.Lock()
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 def exampleJob(worker):
    # time.sleep(0.05)
     h=urllib3.PoolManager()
-    r=h.request('GET','http://www.lviscampuscare.in').read()
+    r=h.request('GET',host).read()
    #time.sleep(0.05)
     with print_lock:
         print(threading.current_thread().name,worker)
